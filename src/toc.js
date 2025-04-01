2fd00071aa91c4fdd3597510d6897fe16a924a47
Momoyeah <zcx23@mails.tsinghua.edu.cn>
2025-04-01 10:35:46 +0800

    Create toc.js

---
+++ src/toc.js
@@ -0,0 +1,10 @@
+export const toc = [
+  {
+    title: "心智的演化与发展",
+    children: [
+      { title: "概述", path: "content/心智的演化与发展/概述.md" },
+      { title: "感受", path: "content/心智的演化与发展/感受.md" },
+      { title: "推理", path: "content/心智的演化与发展/推理.md" }
+    ]
+  }
+]
